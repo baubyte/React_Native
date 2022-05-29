7e290425422246c0762c92a10076201b605e2c7f
baubyte <paredbaez.martin@gmail.com>
2022-05-29 17:48:18 -0300

    box object model

--- 02_Mi_Primer_App/src/Screens/BoxObjectModelScreen.tsx
+++ 02_Mi_Primer_App/src/Screens/BoxObjectModelScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
-      <Text>BoxObjectModel</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Box Object Model</Text>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    //padding: 50,
+    paddingHorizontal: 100,
+    paddingVertical: 20,
+    fontSize: 20,
+    marginHorizontal: 20,
+    marginVertical: 20,
+    //width: 150,
+    borderWidth: 10,
+    //backgroundColor: 'red',
+  },
+});
