1da38141dbe39dcacb60c60748079ad36aaa9e0b
baubyte <paredbaez.martin@gmail.com>
2022-05-29 18:49:20 -0300

    PositionRelativeScreen

--- 02_Mi_Primer_App/App.tsx
+++ 02_Mi_Primer_App/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import {BoxObjectModelScreen} from './src/Screens/BoxObjectModelScreen';
+/* import {BoxObjectModelScreen} from './src/Screens/BoxObjectModelScreen'; */
+/* import {DimensionsScreen} from './src/Screens/DimensionsScreen'; */
+import { PositionRelativeScreen } from './src/Screens/PositionRelativeScreen';
 /* import {CounterScreen} from './src/Screens/CounterScreen'; */
 
-export const App = () => {
-  return (
-    <SafeAreaView>
-      <>
-        {/* <CounterScreen /> */}
-        <BoxObjectModelScreen />
-      </>
-    </SafeAreaView>
-  );
-};
+export const App = () => (
+  <SafeAreaView style={{ flex: 1 }}>
+    {/* <CounterScreen /> */}
+    {/* <BoxObjectModelScreen /> */}
+    {/* <DimensionsScreen /> */}
+    <PositionRelativeScreen/>
+  </SafeAreaView>
+);
