4940d839dd05b6c7bc19cc5f3b5354b3498b231e
baubyte <paredbaez.martin@gmail.com>
2022-06-04 20:09:00 -0300

    wrap

--- 02_Mi_Primer_App/src/Screens/FlexScreen.tsx
+++ 02_Mi_Primer_App/src/Screens/FlexScreen.tsx
@@ -7,6 +7,24 @@ export const FlexScreen = () => {
       <Text style={styles.box1}>Caja 1</Text>
       <Text style={styles.box2}>Caja 2</Text>
       <Text style={styles.box3}>Caja 3</Text>
+      <Text style={styles.box1}>Caja 1</Text>
+      <Text style={styles.box2}>Caja 2</Text>
+      <Text style={styles.box3}>Caja 3</Text>
+      <Text style={styles.box1}>Caja 1</Text>
+      <Text style={styles.box2}>Caja 2</Text>
+      <Text style={styles.box3}>Caja 3</Text>
+      <Text style={styles.box1}>Caja 1</Text>
+      <Text style={styles.box2}>Caja 2</Text>
+      <Text style={styles.box3}>Caja 3</Text>
+      <Text style={styles.box1}>Caja 1</Text>
+      <Text style={styles.box2}>Caja 2</Text>
+      <Text style={styles.box3}>Caja 3</Text>
+      <Text style={styles.box1}>Caja 1</Text>
+      <Text style={styles.box2}>Caja 2</Text>
+      <Text style={styles.box3}>Caja 3</Text>
+      <Text style={styles.box1}>Caja 1</Text>
+      <Text style={styles.box2}>Caja 2</Text>
+      <Text style={styles.box3}>Caja 3</Text>
     </View>
   );
 };
@@ -14,25 +32,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
-    //flexDirection: 'row', //column es por defecto
-    //justifyContent: 'space-between',
+    flexDirection: 'row', //column es por defecto
+    justifyContent: 'center',
+    //alignItems: 'center',
+    //flexWrap: 'wrap',
   },
   box1: {
     //flex: 1,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
+    //alignSelf: 'center', //para sobrescribir un comportamiento del padre
   },
   box2: {
     // flex: 2,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
+    //alignSelf: 'flex-start', //para sobrescribir un comportamiento del padre
   },
   box3: {
     // flex: 1,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
+    //alignSelf: 'flex-end', //para sobrescribir un comportamiento del padre
   },
 });
