8287efba1b1d582bc83d045fa16ae08cdc199395
baubyte <paredbaez.martin@gmail.com>
2022-06-05 16:11:42 -0300

    Pantalla calculdora

---
+++ 03_Calculator/src/Screens/CalculatorScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from '../Theme/appTheme';
+
+export const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Calculadora</Text>
+    </View>
+  );
+};
