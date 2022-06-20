4027bee6d7fc35b9c3320ee75759321cea40c49a
baubyte <paredbaez.martin@gmail.com>
2022-06-20 18:07:44 -0300

    context

--- 04_NavigationApp/App.tsx
+++ 04_NavigationApp/App.tsx
@@ -2,16 +2,28 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {SideMenu} from './src/Navigator/SideMenu';
+import {AuthProvider} from './src/Context/AuthContext/AuthContext';
 /* import {Tabs} from './src/Navigator/Tabs'; */
 /* import {StackNavigator} from './src/Navigator/StackNavigator'; */
 /* import {SideMenuBasic} from './src/Navigator/SideMenuBasic'; */
 
 const App = () => (
   <NavigationContainer>
-    {/* <StackNavigator /> */}
-    {/* <SideMenuBasic /> */}
-    <SideMenu />
-    {/* <Tabs /> */}
+    <AppState>
+      {/* <StackNavigator /> */}
+      {/* <SideMenuBasic /> */}
+      <SideMenu />
+      {/* <Tabs /> */}
+    </AppState>
   </NavigationContainer>
 );
+
+const AppState = ({children}: any) => {
+  return (
+    <AuthProvider>
+      {/** Mostramos los componentes hijos*/}
+      {children}
+    </AuthProvider>
+  );
+};
 export default App;
---
+++ 04_NavigationApp/src/Context/AuthContext/AuthContext.tsx
@@ -0,0 +1,44 @@
+import React, {createContext} from 'react';
+
+/**
+ * Definir que información tendrá
+ */
+export interface AuthState {
+  isLoggedIn: boolean;
+  userName?: string;
+  favoriteIcon?: string;
+}
+/**
+ * Estado Inicial
+ */
+export const AuthInitialState: AuthState = {
+  isLoggedIn: false,
+  userName: undefined,
+  favoriteIcon: undefined,
+};
+/**
+ * Determina como luce y que expone el Contexto
+ */
+export interface AuthContextProps {
+  authState: AuthState;
+  signIn: () => void;
+}
+/**
+ * Creamos el Contexto
+ */
+export const AuthContext = createContext({} as AuthContextProps);
+/**
+ * Proveedor del Estado
+ */
+export const AuthProvider = ({children}: {children: JSX.Element[]}) => {
+  return (
+    <AuthContext.Provider
+      value={{
+        authState: AuthInitialState,
+        signIn: () => {},
+      }}>
+      {/** Mostramos los componentes hijos*/}
+      {children}
+    </AuthContext.Provider>
+  );
+};
