974d81f67a842d67b1ce6d87fae3ea7d00f656e6
baubyte <paredbaez.martin@gmail.com>
2022-06-24 13:55:54 -0300

    boton con icono

---
+++ 04_NavigationApp/src/Components/TouchableIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {colors} from '../Theme/appTheme';
+
+/**
+ * Props
+ */
+interface Props {
+  iconName: string;
+}
+export const TouchableIcon = ({iconName}: Props) => {
+  return (
+    <TouchableOpacity onPress={() => console.log(iconName)}>
+      <Icon name={iconName} size={80} color={colors.primary} />
+    </TouchableOpacity>
+  );
+};
