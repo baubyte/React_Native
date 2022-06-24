1bb207361fa08b7677cf23029ffc0a58b0e2564a
baubyte <paredbaez.martin@gmail.com>
2022-06-24 13:45:00 -0300

    iniciar sesion

--- 04_NavigationApp/src/Screens/ContactsScreen.tsx
+++ 04_NavigationApp/src/Screens/ContactsScreen.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {Button, Text, View} from 'react-native';
+import {AuthContext} from '../Context/AuthContext/AuthContext';
+import {styles} from '../Theme/appTheme';
 
 export const ContactsScreen = () => {
+  const {
+    signIn,
+    authState: {isLoggedIn},
+  } = useContext(AuthContext);
   return (
-    <View>
-      <Text>ContactsScreen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>ContactsScreen</Text>
+      {!isLoggedIn && <Button title="Iniciar Sesión" onPress={signIn} />}
     </View>
   );
 };
