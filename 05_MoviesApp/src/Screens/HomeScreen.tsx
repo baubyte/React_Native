efb63b5aca181bcf793543a47469b8e7d1578a34
baubyte <paredbaez.martin@gmail.com>
2022-07-03 17:07:49 -0300

    carrusel

--- 05_MoviesApp/src/Screens/HomeScreen.tsx
+++ 05_MoviesApp/src/Screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ActivityIndicator, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Carousel from 'react-native-snap-carousel';
 import {MoviePoster} from '../Components/MoviePoster';
 import {useMovies} from '../Hooks/useMovies';
 import {styles} from '../Theme/appTheme';
@@ -8,6 +9,7 @@ import {styles} from '../Theme/appTheme';
 export const HomeScreen = () => {
   const {moviesInCinema, isLoading} = useMovies();
   const {top: marginTop} = useSafeAreaInsets();
+  
   if (isLoading) {
     return (
       <View style={styles.activityIndicator}>
@@ -17,7 +19,13 @@ export const HomeScreen = () => {
   }
   return (
     <View style={{marginTop: marginTop + 20}}>
-      <MoviePoster movie={moviesInCinema[2]} />
+      {/* <MoviePoster movie={moviesInCinema[2]} /> */}
+      <Carousel
+        data={moviesInCinema}
+        renderItem={() => <MoviePoster movie={moviesInCinema[2]} />}
+        sliderWidth={350}
+        itemWidth={300}
+      />
     </View>
   );
 };
