482a6310cf3b81dea18539711f8a52cb3f20cb74
baubyte <paredbaez.martin@gmail.com>
2022-07-06 19:00:30 -0300

    mostrando detalle

--- 05_MoviesApp/src/Screens/DetailScreen.tsx
+++ 05_MoviesApp/src/Screens/DetailScreen.tsx
@@ -1,6 +1,7 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {
+  ActivityIndicator,
   Dimensions,
   Image,
   ScrollView,
@@ -9,8 +10,11 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import currencyFormatter from 'currency-formatter';
 import {RootStackParams} from '../Navigation/Navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {useMovieDetails} from '../Hooks/useMovieDetails';
+import {styles} from '../Theme/appTheme';
 
 const screenHeight = Dimensions.get('screen').height;
 interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
@@ -18,20 +22,40 @@ interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
 export const DetailScreen = ({route}: Props) => {
   const movie = route.params;
   const uri = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
-  
+  const {isLoading, cast, movieFull} = useMovieDetails(movie.id);
+  if (isLoading) {
+    return (
+      <View style={styles.activityIndicator}>
+        <ActivityIndicator color="red" size={100} />
+      </View>
+    );
+  }
   return (
     <ScrollView>
-      <View style={styles.imageContainer}>
-        <View style={styles.imageBorder}>
-          <Image source={{uri}} style={styles.image} />
+      <View style={detailStyles.imageContainer}>
+        <View style={detailStyles.imageBorder}>
+          <Image source={{uri}} style={detailStyles.image} />
         </View>
       </View>
-      <View style={styles.marginContainer}>
-        <Text style={styles.subTitle}>{movie.original_title}</Text>
-        <Text style={styles.title}>{movie.title}</Text>
+      <View style={detailStyles.marginContainer}>
+        <Text style={detailStyles.subTitle}>{movie.original_title}</Text>
+        <Text style={detailStyles.title}>{movie.title}</Text>
+        {/* Historia */}
+        <Text style={detailStyles.titleDetails}>Historia</Text>
+        <Text style={detailStyles.history}>{movieFull!.overview}</Text>
+        {/* Presupuesto */}
+        <Text style={detailStyles.titleDetails}>Presupuesto</Text>
+        <Text style={detailStyles.budget}>
+          {currencyFormatter.format(movieFull!.budget, {
+            locale: 'es-AR',
+            code: 'USD',
+            format: '%s %v',
+          })}
+        </Text>
       </View>
+
       {/* Botón para volver */}
-      <View style={styles.backButton}>
+      <View style={detailStyles.backButton}>
         <TouchableOpacity>
           <Icon color="white" name="arrow-back-outline" size={60} />
         </TouchableOpacity>
@@ -39,7 +63,7 @@ export const DetailScreen = ({route}: Props) => {
     </ScrollView>
   );
 };
-const styles = StyleSheet.create({
+const detailStyles = StyleSheet.create({
   imageContainer: {
     width: '100%',
     height: screenHeight * 0.7,
@@ -82,4 +106,11 @@ const styles = StyleSheet.create({
     top: 30,
     left: 5,
   },
+  titleDetails: {
+    fontSize: 23,
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+  history: {fontSize: 16},
+  budget: {fontSize: 18},
 });
