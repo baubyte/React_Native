65863f916b3839f3975a30e0c23f3e4e7516ff61
baubyte <paredbaez.martin@gmail.com>
2022-07-10 19:13:12 -0300

    agregando el context

--- 05_MoviesApp/App.tsx
+++ 05_MoviesApp/App.tsx
@@ -2,13 +2,25 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/Navigation/Navigation';
+import {GradientProvider} from './src/Context/GradientContext';
 /* import {FadeScreen} from './src/Screens/FadeScreen'; */
+
+const AppState = ({children}: {children: JSX.Element[] | JSX.Element}) => {
+  return (
+    <GradientProvider>
+      {/** Mostramos los componentes hijos*/}
+      {children}
+    </GradientProvider>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
-      {/* <FadeScreen /> */}
+      <AppState>
+        <Navigation />
+        {/* <FadeScreen /> */}
+      </AppState>
     </NavigationContainer>
   );
 };
