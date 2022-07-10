674cf063e679c2d8e95c0bee69b4f1b40d70e3d2
baubyte <paredbaez.martin@gmail.com>
2022-07-10 19:24:02 -0300

    gradiente color poster

--- 05_MoviesApp/src/Components/GradientBackground.tsx
+++ 05_MoviesApp/src/Components/GradientBackground.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import {GradientContext} from '../Context/GradientContext';
 interface Props {
   children: JSX.Element | JSX.Element[];
 }
 
 export const GradientBackground = ({children}: Props) => {
+  const {colors} = useContext(GradientContext);
   return (
     <View style={gradientBackgroundStyles.container}>
       <LinearGradient
-        colors={['#084F6A', '#75CEDB', 'white']}
+        colors={[colors.primaryColor, colors.secondaryColor, 'white']}
         style={{...StyleSheet.absoluteFillObject}}
         start={{x: 0.1, y: 0.1}}
         end={{x: 0.5, y: 0.7}}
--- 05_MoviesApp/src/Screens/HomeScreen.tsx
+++ 05_MoviesApp/src/Screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {ActivityIndicator, Dimensions, ScrollView, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
@@ -7,11 +7,22 @@ import {HorizontalSlider} from '../Components/HorizontalSlider';
 import {MoviePoster} from '../Components/MoviePoster';
 import {useMovies} from '../Hooks/useMovies';
 import {styles} from '../Theme/appTheme';
+import {getImageColors} from '../Helpers/getColors';
+import {GradientContext} from '../Context/GradientContext';
 
 export const HomeScreen = () => {
   const {nowPlaying, popular, topRated, upcoming, isLoading} = useMovies();
   const {top: marginTop} = useSafeAreaInsets();
+  const {setMainColors} = useContext(GradientContext);
+
   const {width: windowWidth} = Dimensions.get('window');
+  const getPosterColors = async (index: number) => {
+    const movie = nowPlaying[index];
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const [primaryColor = 'green', secondaryColor = 'orange'] =
+      await getImageColors(uri);
+    setMainColors({primaryColor, secondaryColor});
+  };
   if (isLoading) {
     return (
       <View style={styles.activityIndicator}>
@@ -31,6 +42,7 @@ export const HomeScreen = () => {
               sliderWidth={windowWidth}
               itemWidth={300}
               inactiveSlideOpacity={0.7}
+              onBeforeSnapToItem={index => getPosterColors(index)}
             />
           </View>
           {/* Películas Populares */}
