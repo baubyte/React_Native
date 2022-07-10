0df546b8167f4e066a8fd1d10cc63b4ae4f47a05
baubyte <paredbaez.martin@gmail.com>
2022-07-10 17:38:24 -0300

    aplicando componente gradiente

--- 05_MoviesApp/src/Screens/HomeScreen.tsx
+++ 05_MoviesApp/src/Screens/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {ActivityIndicator, Dimensions, ScrollView, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import {GradientBackground} from '../Components/GradientBackground';
 import {HorizontalSlider} from '../Components/HorizontalSlider';
 import {MoviePoster} from '../Components/MoviePoster';
 import {useMovies} from '../Hooks/useMovies';
@@ -19,25 +20,27 @@ export const HomeScreen = () => {
     );
   }
   return (
-    <ScrollView>
-      <View style={{marginTop: marginTop + 20}}>
-        {/* Carrusel películas en cine */}
-        <View style={styles.carouselContainer}>
-          <Carousel
-            data={nowPlaying}
-            renderItem={({item}: any) => <MoviePoster movie={item} />}
-            sliderWidth={windowWidth}
-            itemWidth={300}
-            inactiveSlideOpacity={0.7}
-          />
+    <GradientBackground>
+      <ScrollView>
+        <View style={{marginTop: marginTop + 20}}>
+          {/* Carrusel películas en cine */}
+          <View style={styles.carouselContainer}>
+            <Carousel
+              data={nowPlaying}
+              renderItem={({item}: any) => <MoviePoster movie={item} />}
+              sliderWidth={windowWidth}
+              itemWidth={300}
+              inactiveSlideOpacity={0.7}
+            />
+          </View>
+          {/* Películas Populares */}
+          <HorizontalSlider title="Populares" movies={popular} />
+          {/* Películas Mejor Calificadas" */}
+          <HorizontalSlider title="Mejor Calificadas" movies={topRated} />
+          {/* Películas Próximamente" */}
+          <HorizontalSlider title="Próximamente" movies={upcoming} />
         </View>
-        {/* Películas Populares */}
-        <HorizontalSlider title="Populares" movies={popular} />
-        {/* Películas Mejor Calificadas" */}
-        <HorizontalSlider title="Mejor Calificadas" movies={topRated} />
-        {/* Películas Próximamente" */}
-        <HorizontalSlider title="Próximamente" movies={upcoming} />
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </GradientBackground>
   );
 };
