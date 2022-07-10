f55a9230c636b5d4ea4d989abb36e61d2e718b34
baubyte <paredbaez.martin@gmail.com>
2022-07-10 17:25:12 -0300

    fade screen prueba

--- 05_MoviesApp/App.tsx
+++ 05_MoviesApp/App.tsx
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/Navigation/Navigation';
+/* import {FadeScreen} from './src/Screens/FadeScreen'; */
 
 const App = () => {
   return (
     <NavigationContainer>
       <Navigation />
+      {/* <FadeScreen /> */}
     </NavigationContainer>
   );
 };
