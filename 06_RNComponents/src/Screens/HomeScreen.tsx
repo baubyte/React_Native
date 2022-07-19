3549c040f5e18c781b4657bb05ec37380d45fd2b
baubyte <paredbaez.martin@gmail.com>
2022-07-19 18:14:01 -0300

    flatlist

--- 06_RNComponents/src/Screens/HomeScreen.tsx
+++ 06_RNComponents/src/Screens/HomeScreen.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+
+interface MenuItem {
+  name: string;
+  iconName: string;
+  component: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    iconName: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+];
 
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>
+          {menuItem.iconName} - {menuItem.name}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Icon name="arrow-back-outline" size={30} color="red" />
-      <Text>HomeScreen</Text>
+    <View style={homeStyles.container}>
+      <FlatList
+        data={menuItems}
+        renderItem={({item}) => renderMenuItem(item)}
+        keyExtractor={item => item.component}
+      />
     </View>
   );
 };
+const homeStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
