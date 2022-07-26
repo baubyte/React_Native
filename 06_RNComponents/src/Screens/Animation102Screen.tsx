45ecef38337ad4c8b626b74f8af0b3efe3d3c0bd
baubyte <paredbaez.martin@gmail.com>
2022-07-26 17:36:57 -0300

    animaciones

--- 06_RNComponents/src/Screens/Animation102Screen.tsx
+++ 06_RNComponents/src/Screens/Animation102Screen.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {Animated, PanResponder, StyleSheet, View} from 'react-native';
 
 export const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}], {
+      useNativeDriver: false,
+    }),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: false}, // Back to zero
+      ).start();
+    },
+  });
+
   return (
     <View style={internalStyles.container}>
-      <View style={internalStyles.purpleBox} />
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), internalStyles.box]}
+      />
     </View>
   );
 };
 const internalStyles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  purpleBox: {
-    backgroundColor: '#5856D6',
+  box: {
+    backgroundColor: '#75CEDE',
     width: 150,
     height: 150,
+    borderRadius: 4,
   },
 });
