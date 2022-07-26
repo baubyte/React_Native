20ee9bf6d90b571c4c75681b6c4d261217f57bd3
baubyte <paredbaez.martin@gmail.com>
2022-07-26 17:23:32 -0300

    animaciones

--- 06_RNComponents/src/Screens/Animation101Screen.tsx
+++ 06_RNComponents/src/Screens/Animation101Screen.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Animated, Button, StyleSheet, View} from 'react-native';
+import {useAnimation} from '../Hooks/useAnimation';
 
 export const Animation101Screen = () => {
+  const {fadeIn, fadeOut, opacity, position, startMovingPosition} =
+    useAnimation();
   return (
     <View style={internalStyles.container}>
-      <View style={internalStyles.purpleBox} />
+      <Animated.View
+        style={{
+          ...internalStyles.purpleBox,
+          opacity,
+          transform: [
+            {
+              translateY: position,
+            },
+          ],
+        }}
+      />
+      <Button
+        onPress={() => {
+          fadeIn();
+          startMovingPosition(-100);
+        }}
+        title="Fade In"
+      />
+      <Button onPress={() => fadeOut()} title="Fade Out" />
     </View>
   );
 };
 const internalStyles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   purpleBox: {
     backgroundColor: '#5856D6',
