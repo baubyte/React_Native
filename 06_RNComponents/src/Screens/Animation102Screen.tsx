b91b3a6ff092e02323ba018f56ccae461cca6aaa
baubyte <paredbaez.martin@gmail.com>
2022-07-26 15:35:48 -0300

    nuevos compponentes

---
+++ 06_RNComponents/src/Screens/Animation102Screen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+export const Animation102Screen = () => {
+  return (
+    <View style={internalStyles.container}>
+      <View style={internalStyles.purpleBox} />
+    </View>
+  );
+};
+const internalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  purpleBox: {
+    backgroundColor: '#5856D6',
+    width: 150,
+    height: 150,
+  },
+});
