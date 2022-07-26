a2ccb1b0a1e78db35efa26aa4d6877e2c4fd8e60
baubyte <paredbaez.martin@gmail.com>
2022-07-26 13:33:26 -0300

    TouchableOpacity items

--- 06_RNComponents/src/Components/FlatListMenuItem.tsx
+++ 06_RNComponents/src/Components/FlatListMenuItem.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {MenuItem} from '../Interfaces/appInterfaces';
+import {Spacer} from './Spacer';
 interface Props {
   menuItem: MenuItem;
 }
 
 export const FlatListMenuItem = ({menuItem}: Props) => {
   return (
-    <View style={styles.container}>
-      <Icon name={menuItem.iconName} color="gray" size={24} />
-      <Text style={styles.itemText}>{menuItem.name}</Text>
-      <Icon name="chevron-forward-outline" color="gray" size={24} />
-    </View>
+    <TouchableOpacity activeOpacity={0.8}>
+      <View style={styles.container}>
+        <Icon name={menuItem.iconName} color="gray" size={24} />
+        <Text style={styles.itemText}>{menuItem.name}</Text>
+        <Spacer />
+        <Icon name="chevron-forward-outline" color="gray" size={24} />
+      </View>
+    </TouchableOpacity>
   );
 };
 const styles = StyleSheet.create({
