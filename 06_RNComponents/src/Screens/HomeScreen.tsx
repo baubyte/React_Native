501fd40e196663ff07d56f7d8eebdb12875d0234
baubyte <paredbaez.martin@gmail.com>
2022-07-26 13:24:23 -0300

    componente flat list

--- 06_RNComponents/src/Screens/HomeScreen.tsx
+++ 06_RNComponents/src/Screens/HomeScreen.tsx
@@ -1,37 +1,43 @@
 import React from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {FlatListMenuItem} from '../Components/FlatListMenuItem';
+import {MenuItem} from '../Interfaces/appInterfaces';
+import {styles} from '../Theme/appTheme';
 
-interface MenuItem {
-  name: string;
-  iconName: string;
-  component: string;
-}
-
-const menuItems = [
+const menuItems: MenuItem[] = [
   {
     name: 'Animation 101',
     iconName: 'cube-outline',
     component: 'Animation101Screen',
   },
+  {
+    name: 'Animation 102',
+    iconName: 'albums-outline',
+    component: 'Animation102Screen',
+  },
 ];
 
 export const HomeScreen = () => {
-  const renderMenuItem = (menuItem: MenuItem) => {
+  const {top} = useSafeAreaInsets();
+  const renderListHeader = () => {
     return (
-      <View>
-        <Text>
-          {menuItem.iconName} - {menuItem.name}
-        </Text>
+      <View style={{...homeStyles.headerListContainer, marginTop: top + 20}}>
+        <Text style={styles.title}>Opciones de Menú</Text>
       </View>
     );
   };
-
+  const ItemSeparator = () => {
+    return <View style={homeStyles.itemSeparator} />;
+  };
   return (
-    <View style={homeStyles.container}>
+    <View style={(homeStyles.container, styles.globalMargin)}>
       <FlatList
         data={menuItems}
-        renderItem={({item}) => renderMenuItem(item)}
+        renderItem={({item}) => <FlatListMenuItem menuItem={item}/>}
         keyExtractor={item => item.component}
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={ItemSeparator}
       />
     </View>
   );
@@ -39,5 +45,13 @@ export const HomeScreen = () => {
 const homeStyles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerListContainer: {
+    marginBottom: 20,
+  },
+  itemSeparator: {
+    borderBottomWidth: 1,
+    opacity: 0.4,
+    marginVertical: 8,
   },
 });
