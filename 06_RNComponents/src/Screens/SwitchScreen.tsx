6009aaddbae46cb7de3aa0aa8eac82e9a47b50cd
baubyte <paredbaez.martin@gmail.com>
2022-07-28 19:22:05 -0300

    pantalla de los switch

--- 06_RNComponents/src/Screens/SwitchScreen.tsx
+++ 06_RNComponents/src/Screens/SwitchScreen.tsx
@@ -1,23 +1,69 @@
 import React, {useState} from 'react';
-import {Platform, StyleSheet, Switch, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {CustomSwitch} from '../Components/CustomSwitch';
+import {HeaderTitle} from '../Components/HeaderTitle';
+import {Spacer} from '../Components/Spacer';
 
 export const SwitchScreen = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(!isEnabled);
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+  /**
+   *
+   * @param value
+   * @param field
+   */
+  const onChange = (value: boolean, field: keyof typeof state) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+  };
+  const {isActive, isHungry, isHappy} = state;
   return (
     <View style={internalStyles.container}>
-      <Switch
-        trackColor={{false: '#D9D9DB', true: '#5856D6'}}
-        thumbColor={Platform.OS === 'android' ? '#5856D6' : ''}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+      <HeaderTitle title="Switches" />
+      <View style={internalStyles.switchRow}>
+        <Text style={internalStyles.switchText}>isActive</Text>
+        <Spacer />
+        <CustomSwitch
+          isOn={isActive}
+          onChange={value => onChange(value, 'isActive')}
+        />
+      </View>
+      <View style={internalStyles.switchRow}>
+        <Text style={internalStyles.switchText}>isHungry</Text>
+        <Spacer />
+        <CustomSwitch
+          isOn={isHungry}
+          onChange={value => onChange(value, 'isHungry')}
+        />
+      </View>
+      <View style={internalStyles.switchRow}>
+        <Text style={internalStyles.switchText}>isHappy</Text>
+        <Spacer />
+        <CustomSwitch
+          isOn={isHappy}
+          onChange={value => onChange(value, 'isHappy')}
+        />
+      </View>
+      <Text style={internalStyles.switchText}>
+        {JSON.stringify(state, null, 6)}
+      </Text>
     </View>
   );
 };
 const internalStyles = StyleSheet.create({
   container: {
-    marginTop: 100,
+    marginHorizontal: 20,
+  },
+  switchRow: {
+    flexDirection: 'row',
+    marginVertical: 10,
+  },
+  switchText: {
+    fontSize: 25,
   },
 });
