f506ebe6e87508425232ceaf2194d8ab6006165c
baubyte <paredbaez.martin@gmail.com>
2022-07-28 14:58:52 -0300

    Items Menu

---
+++ 06_RNComponents/src/Data/MenuItems.tsx
@@ -0,0 +1,18 @@
+import {MenuItem} from '../Interfaces/appInterfaces';
+export const menuItems: MenuItem[] = [
+  {
+    name: 'Animation 101',
+    iconName: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    iconName: 'albums-outline',
+    component: 'Animation102Screen',
+  },
+  {
+    name: 'Switches',
+    iconName: 'toggle-outline',
+    component: 'SwitchScreen',
+  },
+];
