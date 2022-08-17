9aa9b45cfcf8feaeb5e7ce301dfab54dce3bdb13
baubyte <paredbaez.martin@gmail.com>
2022-08-17 17:28:55 -0300

    inputs

--- 06_RNComponents/src/Screens/TextInputScreen.tsx
+++ 06_RNComponents/src/Screens/TextInputScreen.tsx
@@ -1,14 +1,65 @@
-import React from 'react';
-import {StyleSheet, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import {HeaderTitle} from '../Components/HeaderTitle';
+import {Separator} from '../Components/Separator';
 import {styles} from '../Theme/appTheme';
 
 export const TextInputScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+  const onChange = (value: string, field: string) => {
+    setForm({...form, [field]: value});
+  };
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="Text Inputs" />
-      <TextInput style={internalStyles.input} />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.globalMargin}>
+            <HeaderTitle title="Text Inputs" />
+            <TextInput
+              style={internalStyles.input}
+              placeholder="Ingrese su Nombre"
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={value => onChange(value, 'name')}
+            />
+            <Separator />
+            <TextInput
+              style={internalStyles.input}
+              placeholder="Ingrese su Email"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={value => onChange(value, 'email')}
+              keyboardType="email-address"
+              keyboardAppearance="dark"
+            />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <Separator />
+            <TextInput
+              style={internalStyles.input}
+              placeholder="Ingrese su Teléfono"
+              onChangeText={value => onChange(value, 'phone')}
+              keyboardType="phone-pad"
+            />
+            <Separator height={100} />
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 const internalStyles = StyleSheet.create({
