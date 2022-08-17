b2e05df7e333b8dc37fd7240e33d9e593830c76d
baubyte <paredbaez.martin@gmail.com>
2022-08-17 15:59:35 -0300

    separador

---
+++ 06_RNComponents/src/Components/Separator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+export const Separator = () => {
+  return <View style={styles.separator} />;
+};
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
