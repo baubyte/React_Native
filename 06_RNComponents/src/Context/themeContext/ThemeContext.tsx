5c3cd1b6f50177b16f6863a2e232b9c0be55c596
baubyte <paredbaez.martin@gmail.com>
2022-08-21 18:47:09 -0300

    context theme

---
+++ 06_RNComponents/src/Context/themeContext/ThemeContext.tsx
@@ -0,0 +1,37 @@
+import React, {createContext, useReducer} from 'react';
+import {lightTheme, themeReducer, ThemeState} from './themeReducer';
+
+interface ThemeContextProps {
+  theme: ThemeState;
+  setDarkTheme: () => void;
+  setLightTheme: () => void;
+}
+
+export const ThemeContext = createContext({} as ThemeContextProps);
+
+export const ThemeProvider = ({
+  children,
+}: {
+  children: JSX.Element[] | JSX.Element;
+}) => {
+  //const theme = {};
+  const [theme, dispatch] = useReducer(themeReducer, lightTheme);
+  const setDarkTheme = () => {
+    dispatch({type: 'set_dark_theme'});
+    console.log('setDarkTheme');
+  };
+  const setLightTheme = () => {
+    dispatch({type: 'set_light_theme'});
+    console.log('setLightTheme');
+  };
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        setDarkTheme,
+        setLightTheme,
+      }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
---
+++ 06_RNComponents/src/Context/themeContext/themeReducer.tsx
@@ -0,0 +1,54 @@
+import {Theme} from '@react-navigation/native';
+
+type ThemeAction = {type: 'set_light_theme'} | {type: 'set_dark_theme'};
+
+export interface ThemeState extends Theme {
+  currentTheme: Themes;
+  dividerColor: string;
+}
+/**
+ *
+ */
+enum Themes {
+  dark,
+  light,
+}
+export const lightTheme: ThemeState = {
+  currentTheme: Themes.light,
+  dark: false,
+  dividerColor: 'rgba(0,0,0, 0.6)',
+  colors: {
+    primary: '#084F6A',
+    background: 'white',
+    card: 'white',
+    text: 'black',
+    border: 'black',
+    notification: 'teal',
+  },
+};
+export const darkTheme: ThemeState = {
+  currentTheme: Themes.dark,
+  dark: true,
+  dividerColor: 'rgba(255,255,255, 0.6)',
+  colors: {
+    primary: '#75CEDB',
+    background: 'black',
+    card: 'black',
+    text: 'white',
+    border: 'black',
+    notification: 'teal',
+  },
+};
+export const themeReducer = (
+  state: ThemeState,
+  action: ThemeAction,
+): ThemeState => {
+  switch (action.type) {
+    case 'set_light_theme':
+      return {...lightTheme};
+    case 'set_dark_theme':
+      return {...darkTheme};
+    default:
+      return state;
+  }
+};
