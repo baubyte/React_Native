7094c67a18343c3430cc1d6c9f001a1582dec0b8
baubyte <paredbaez.martin@gmail.com>
2022-08-21 18:47:19 -0300

    usando context

--- 06_RNComponents/App.tsx
+++ 06_RNComponents/App.tsx
@@ -1,30 +1,38 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
-  Theme,
-} from '@react-navigation/native';
 import {Navigation} from './src/Navigation/Navigation';
+import {ThemeProvider} from './src/Context/themeContext/ThemeContext';
 
-const customTheme: Theme = {
+/* const customTheme: Theme = {
   dark: true,
   colors: {
     ...DarkTheme.colors,
-    /* primary: 'string',
+    primary: 'string',
     background: 'black',
     card: 'string',
     text: 'string',
     border: 'string',
-    notification: 'string', */
+    notification: 'string',
   },
-};
+}; */
 const App = () => {
   return (
-    <NavigationContainer theme={customTheme}>
+    <AppState>
       <Navigation />
-    </NavigationContainer>
+    </AppState>
+  );
+};
+/**
+ * Estado de la Aplicacion
+ * @param
+ * @returns JSX
+ */
+const AppState = ({children}: {children: JSX.Element[] | JSX.Element}) => {
+  return (
+    <ThemeProvider>
+      {/** Mostramos los componentes hijos*/}
+      {children}
+    </ThemeProvider>
   );
 };
 export default App;
