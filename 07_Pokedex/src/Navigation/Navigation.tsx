bf4d00e99137660e52dc0f13da0e909045cb0662
baubyte <paredbaez.martin@gmail.com>
2022-08-28 16:26:04 -0300

    navigation sin header

--- 07_Pokedex/src/Navigation/Navigation.tsx
+++ 07_Pokedex/src/Navigation/Navigation.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {View} from 'react-native';
 import {HomeScreen} from '../Screens/HomeScreen';
 import {PokemonScreen} from '../Screens/PokemonScreen';
 
@@ -13,11 +12,15 @@ const Stack = createStackNavigator<RootStackParams>();
 
 export const Navigation = () => {
   return (
-    <View>
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
-      </Stack.Navigator>
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white',
+        },
+      }}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
+    </Stack.Navigator>
   );
 };
