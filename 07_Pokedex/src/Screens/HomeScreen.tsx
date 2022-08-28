f56ab5ed8b3881323fcf4c8263c53ffdd74e7ff4
baubyte <paredbaez.martin@gmail.com>
2022-08-28 18:18:22 -0300

    aplicando fadeimage

--- 07_Pokedex/src/Screens/HomeScreen.tsx
+++ 07_Pokedex/src/Screens/HomeScreen.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ActivityIndicator, FlatList, Image, Text} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {FadeInImage} from '../Components/FadeInImage';
+import {usePokemonPaginated} from '../Hooks/usePokemonPaginated';
+import {styles} from '../Theme/appTheme';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const {simplePokemonList, loadPokemons} = usePokemonPaginated();
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokeBallBackground}
+      />
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => (
+          <FadeInImage uri={item.picture} style={styles.pokemonImage} />
+        )}
+        //Infinite scroll
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={
+          <ActivityIndicator
+            style={styles.listFooterActivityIndicator}
+            size={20}
+            color="grey"
+          />
+        }
+      />
+      {/* <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
+        Pokedex
+      </Text> */}
+    </>
   );
 };
