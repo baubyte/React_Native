626379e44cf432e562d6bda20312117ab41dac3b
baubyte <paredbaez.martin@gmail.com>
2022-08-28 17:37:40 -0300

    hook pokemons paginados

---
+++ 07_Pokedex/src/Hooks/usePokemonPaginated.tsx
@@ -0,0 +1,44 @@
+import {useEffect, useRef, useState} from 'react';
+import {pokemonApi} from '../Api/pokemonApi';
+import {
+  PokemonPaginatedResponse,
+  Result,
+  SimplePokemon,
+} from '../Interfaces/pokemonInterfaces';
+
+export const usePokemonPaginated = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
+    [],
+  );
+  const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon/?limit=40');
+  /**
+   * Obtiene los Pokemons
+   */
+  const loadPokemons = async () => {
+    setIsLoading(true);
+    const response = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current,
+    );
+    nextPageUrl.current = response.data.next;
+    mapPokemonList(response.data.results);
+  };
+  /**
+   * Convierte el result a un tipo SimplePokemon
+   * @param pokemonList
+   */
+  const mapPokemonList = (pokemonList: Result[]) => {
+    const newPokemonList: SimplePokemon[] = pokemonList.map(({name, url}) => {
+      const urlParts = url.split('/');
+      const id = urlParts[urlParts.length - 2];
+      const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+      return {id, picture, name};
+    });
+    setSimplePokemonList([...simplePokemonList, ...newPokemonList]);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    loadPokemons();
+  }, []);
+  return {simplePokemonList, isLoading};
+};
