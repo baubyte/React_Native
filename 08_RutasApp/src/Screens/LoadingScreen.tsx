533f9b79cfe7f2dfd736e8b43978445e93a7507e
baubyte <paredbaez.martin@gmail.com>
2022-09-14 16:14:08 -0300

    pantalla de carga

--- 08_RutasApp/src/Screens/LoadingScreen.tsx
+++ 08_RutasApp/src/Screens/LoadingScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
 
 export const LoadingScreen = () => {
   return (
-    <View>
-      <Text>LoadingScreen</Text>
+    <View style={internalStyles.container}>
+      <ActivityIndicator size={50} color="black" />
     </View>
   );
 };
+const internalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
