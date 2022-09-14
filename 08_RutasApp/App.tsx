b4511f8a6ee43ea484c2985f02429434006bc990
baubyte <paredbaez.martin@gmail.com>
2022-09-14 15:23:53 -0300

    app state

--- 08_RutasApp/App.tsx
+++ 08_RutasApp/App.tsx
@@ -2,11 +2,17 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/Navigation/Navigation';
+import {PermissionsProvider} from './src/Context/Permission/PermissionsContext';
 
+const AppState = ({children}: {children: JSX.Element[] | JSX.Element}) => {
+  return <PermissionsProvider>{children}</PermissionsProvider>;
+};
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
+      <AppState>
+        <Navigation />
+      </AppState>
     </NavigationContainer>
   );
 };
