7218362845f5e71bfb11d21746683cab88d802cf
baubyte <paredbaez.martin@gmail.com>
2022-09-30 13:58:05 -0300

    paquete geolocation

---
+++ 08_RutasApp/src/Components/Map.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import MapView, {Marker} from 'react-native-maps';
+
+interface Props {
+  markers?: typeof Marker[];
+}
+
+export const Map = ({markers}: Props) => {
+  return (
+    <>
+      <MapView
+        style={{flex: 1}}
+        showsUserLocation
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        {/* <Marker
+          image={require('../assets/customMarker.png')}
+          coordinate={{latitude: 37.78825, longitude: -122.4324}}
+          title="Titulo Marker"
+          description="Descripción Marker"
+        /> */}
+      </MapView>
+    </>
+  );
+};
