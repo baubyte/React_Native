0a0a083128eedc767ebcc63bf64b2bb8a531f27e
baubyte <paredbaez.martin@gmail.com>
2022-10-12 17:07:40 -0300

    current location

--- 08_RutasApp/src/Hooks/useLocation.tsx
+++ 08_RutasApp/src/Hooks/useLocation.tsx
@@ -1,29 +1,60 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import Geolocation from '@react-native-community/geolocation';
 import {Location} from '../Interfaces/appInterfaces';
 
 export const useLocation = () => {
   const [hasLocation, setHasLocation] = useState<boolean>(false);
   const [initialPosition, setInitialPosition] = useState<Location>();
+  const [userLocation, setUserLocation] = useState<Location>({
+    longitude: 0,
+    latitude: 0,
+  });
+  const watchId = useRef<number>();
   useEffect(() => {
-    Geolocation.getCurrentPosition(
+    getCurrentLocation().then(location => {
+      setInitialPosition(location);
+      setUserLocation(location);
+      setHasLocation(true);
+    });
+  }, []);
+
+  const getCurrentLocation = (): Promise<Location> => {
+    return new Promise((resolve, reject) => {
+      Geolocation.getCurrentPosition(
+        ({coords}) => {
+          resolve({
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+          });
+        },
+        error => reject({error}),
+        {enableHighAccuracy: true},
+      );
+    });
+  };
+  const followLocation = () => {
+    watchId.current = Geolocation.watchPosition(
       ({coords}) => {
-        setInitialPosition({
+        setUserLocation({
           latitude: coords.latitude,
           longitude: coords.longitude,
         });
-        setHasLocation(true);
-        return () => {
-          Geolocation;
-        };
       },
       error => console.log({error}),
-      {enableHighAccuracy: true},
+      {enableHighAccuracy: true, distanceFilter: 10},
     );
-  }, []);
-
+  };
+  const stopFollowingUserLocation = () => {
+    if (watchId.current) {
+      Geolocation.clearWatch(watchId.current);
+    }
+  };
   return {
     hasLocation,
     initialPosition,
+    getCurrentLocation,
+    userLocation,
+    followLocation,
+    stopFollowingUserLocation,
   };
 };
