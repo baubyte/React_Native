7954c595028e4ef4f83cd3104332e133979bf48e
baubyte <paredbaez.martin@gmail.com>
2022-10-23 18:58:42 -0300

    uso context

--- 10_ProductosApp/App.tsx
+++ 10_ProductosApp/App.tsx
@@ -2,11 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/Navigation/Navigation';
+import {AuthProvider} from './src/Context/Auth/AuthContext';
+
+const AppState = ({children}: {children: JSX.Element[] | JSX.Element}) => {
+  return <AuthProvider>{children}</AuthProvider>;
+};
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
+      <AppState>
+        <Navigation />
+      </AppState>
     </NavigationContainer>
   );
 };
