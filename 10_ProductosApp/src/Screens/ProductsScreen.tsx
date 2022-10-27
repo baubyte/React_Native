5549dee4cf14e7ac29b93bc2fd9b36aed812c3f8
baubyte <paredbaez.martin@gmail.com>
2022-10-27 20:32:03 -0300

    listado de productos

--- 10_ProductosApp/src/Screens/ProductsScreen.tsx
+++ 10_ProductosApp/src/Screens/ProductsScreen.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {ItemSeparator} from '../Components/ItemSeparator';
+import {ProductContext} from '../Context/Product/ProductContext';
 
 export const ProductsScreen = () => {
+  const {products, loadProducts} = useContext(ProductContext);
+  //TODO: Pull to refresh
   return (
-    <View>
-      <Text>ProductsScreen</Text>
+    <View style={internalStyles.container}>
+      <FlatList
+        data={products}
+        keyExtractor={product => product._id}
+        renderItem={({item}) => (
+          <TouchableOpacity activeOpacity={0.7}>
+            <Text style={internalStyles.productName}>{item.nombre}</Text>
+          </TouchableOpacity>
+        )}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+      />
     </View>
   );
 };
+const internalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+  },
+  productName: {
+    fontSize: 20,
+  },
+});
