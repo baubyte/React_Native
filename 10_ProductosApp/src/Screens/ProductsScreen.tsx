3f3614d46583c26f6de8d982c7846224054533f6
baubyte <paredbaez.martin@gmail.com>
2022-11-06 19:18:12 -0300

    navegacion a producto

--- 10_ProductosApp/src/Screens/ProductsScreen.tsx
+++ 10_ProductosApp/src/Screens/ProductsScreen.tsx
@@ -1,10 +1,28 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {ItemSeparator} from '../Components/ItemSeparator';
 import {ProductContext} from '../Context/Product/ProductContext';
+import {StackScreenProps} from '@react-navigation/stack';
+import {ProductsStackParams} from '../Navigation/ProductsNavigation';
 
-export const ProductsScreen = () => {
+interface Props
+  extends StackScreenProps<ProductsStackParams, 'ProductsScreen'> {}
+
+export const ProductsScreen = ({navigation}: Props) => {
   const {products, loadProducts} = useContext(ProductContext);
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={internalStyles.buttonAddProduct}
+          onPress={() => navigation.navigate('ProductScreen', {})}>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
+
   //TODO: Pull to refresh
   return (
     <View style={internalStyles.container}>
@@ -12,7 +30,14 @@ export const ProductsScreen = () => {
         data={products}
         keyExtractor={product => product._id}
         renderItem={({item}) => (
-          <TouchableOpacity activeOpacity={0.7}>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() =>
+              navigation.navigate('ProductScreen', {
+                id: item._id,
+                name: item.nombre,
+              })
+            }>
             <Text style={internalStyles.productName}>{item.nombre}</Text>
           </TouchableOpacity>
         )}
@@ -28,5 +53,8 @@ const internalStyles = StyleSheet.create({
   },
   productName: {
     fontSize: 20,
+  },
+  buttonAddProduct: {
+    marginRight: 10,
   },
 });
