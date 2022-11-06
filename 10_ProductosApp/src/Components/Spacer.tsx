31633386ce180a93bf1f6b4ab95f6ba8bccbcd34
baubyte <paredbaez.martin@gmail.com>
2022-11-06 19:17:49 -0300

    espacio

---
+++ 10_ProductosApp/src/Components/Spacer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+export const Spacer = () => {
+  return <View style={styles.spacer} />;
+};
+const styles = StyleSheet.create({
+  spacer: {
+    width: 10,
+  },
+});
