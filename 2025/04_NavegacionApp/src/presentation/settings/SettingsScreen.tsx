d50c1d04d140eab71fdbff7f84981876f51a1867
baubyte <paredbaez.martin@gmail.com>
2025-06-05 21:21:02 -0300

    Agregar pantalla de configuración con navegación y estilos

--- 2025/04_NavegacionApp/src/presentation/settings/SettingsScreen.tsx
+++ 2025/04_NavegacionApp/src/presentation/settings/SettingsScreen.tsx
@@ -1,9 +1,17 @@
 import { View, Text } from 'react-native';
+import { PrimaryButton } from '../components/shared';
+import { globalStyles } from '../theme/theme';
+import { StackActions, useNavigation } from '@react-navigation/native';
+
 
 export const SettingsScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Configuración</Text>
+      <PrimaryButton label="Regresar" onPress={() => navigation.goBack()} />
+      <PrimaryButton label="Regresar al Home" onPress={() => navigation.dispatch(StackActions.popToTop())} />
     </View>
   );
 };
